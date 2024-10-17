f02e43fd22e9a4368718fa8a4f2afd76f6098482
rohith359 <srohith2584@gmail.com>
2024-10-17 13:52:52 +0530

    Create d4.py

---
+++ d4.py
@@ -0,0 +1,7 @@
+...
+4) Write a program to delete the given element in the given set values. If the given element is not in the set values, then print "Given value is not present in the set list.".
+Sample Input:
+1 2 3 4
+2
+Sample Output:
+1 3 4 
