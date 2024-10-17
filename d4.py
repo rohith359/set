033a766b91a81cdba14d3f60e3df36a4c4ef6691
rohith359 <srohith2584@gmail.com>
2024-10-17 13:54:40 +0530

    Update d4.py

--- d4.py
+++ d4.py
@@ -5,3 +5,12 @@ Sample Input:
 2
 Sample Output:
 1 3 4 
+...
+values = set(map(int, input().split()))
+element_to_delete = int(input())
+
+if element_to_delete in values:
+    values.remove(element_to_delete)
+    print(" ".join(map(str, sorted(values))) + " ")
+else:
+    print("Given value is not present in the set list.")
