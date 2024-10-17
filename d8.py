b0907e796c9f0f1b2356f2f536d8764e1a09ff52
rohith359 <srohith2584@gmail.com>
2024-10-17 14:03:00 +0530

    Create d8.py

---
+++ d8.py
@@ -0,0 +1,13 @@
+....
+8) Write a program to update the given set in another set.
+Sample Input:
+1 2 3
+3 4 5
+Sample Output:
+1 2 3 4 5
+...
+set1 = set(map(int, input().split()))
+set2 = set(map(int, input().split()))
+
+set1.update(set2)
+print(" ".join(map(str, sorted(set1))) + " ")
